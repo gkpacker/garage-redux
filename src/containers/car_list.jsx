3c20fc003938fb751e2418f4fd861677ff0cd8bd
Gabriel Packer <ggkpacker@gmail.com>
2019-04-18 00:35:58 -0300

    Remove car component

--- src/containers/car_list.jsx
+++ src/containers/car_list.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
 
 import { fetchCars } from '../actions';
-import Car from '../components/car';
 
 class CarList extends Component {
   componentWillMount() {
-    const { garageName } = this.props;
+    const { garage } = this.props;
 
-    this.props.fetchCars(garageName);
+    this.props.fetchCars(garage);
+  }
+
+  renderCar = (car) => {
+    return (
+      <Link key={car.id} to={`/cars/${car.id}`} >
+        <li className="car-smallad">
+          <img src="assets/images/kombi.jpg" className="car-logo" alt="" />
+          <div className="car-details">
+            <span>{`${car.brand} - ${car.model}`}</span>
+            <ul>
+              <li>Owner: {car.owner}</li>
+            </ul>
+          </div>
+        </li>
+      </Link>
+    );
   }
 
   render() {
@@ -22,7 +38,7 @@ class CarList extends Component {
     return (
       <div className="list-container">
         <ul>
-          {cars.map(car => <Car key={car.id} car={car} />)}
+          {cars.map(this.renderCar)}
         </ul>
       </div>
     );
@@ -32,7 +48,7 @@ class CarList extends Component {
 function mapStateToProps(state) {
   return {
     cars: state.cars,
-    garageName: state.garageName
+    garage: state.garage
   };
 }
 
