51709d22502de12014c679cf4947ea7bec20a8e8
Gabriel Packer <ggkpacker@gmail.com>
2019-04-18 00:33:20 -0300

    Add create car action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,28 @@
 export const FETCH_CARS = 'FETCH_CARS';
+export const CAR_CREATED = 'CAR_CREATED';
+export const FETCH_CAR = 'FETCH_CAR';
 
-export default function fetchCars(garage) {
+export function fetchCars(garage) {
   const promise = fetch(`https://wagon-garage-api.herokuapp.com/${garage}/cars`)
     .then(response => response.json());
 
   return {
     type: FETCH_CARS,
+    payload: promise
+  };
+}
+
+export function createCar(garage, body, callback) {
+  const promise = fetch(
+    `https://wagon-garage-api.herokuapp.com/${garage}/cars`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
+    }).then(response => response.json())
+    .then(callback);
+
+  return {
+    type: CAR_CREATED,
     payload: promise
   };
 }
